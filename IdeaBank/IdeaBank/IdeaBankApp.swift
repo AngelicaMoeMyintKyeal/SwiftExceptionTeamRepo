39e0ccefb25063a2a07c50cb97a2f200930d1ee5
Htet Myat Moe Myint Kyeal <htetmyak.m.m.kyal@gmail.com>
2023-01-11 09:06:37 +0100

    Initiated a new project

---
+++ IdeaBank/IdeaBank/IdeaBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdeaBankApp.swift
+//  IdeaBank
+//
+//  Created by Htet Myat Moe Myint Kyeal on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct IdeaBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
