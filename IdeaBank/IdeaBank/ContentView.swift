c5fc0be93a39c2b3872398bf43eb9bfa63678fe7
shinra-electric <50119606+shinra-electric@users.noreply.github.com>
2023-01-14 16:32:36 +0100

    Initial test for tinder swipe

--- IdeaBank/IdeaBank/ContentView.swift
+++ IdeaBank/IdeaBank/ContentView.swift
@@ -9,10 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var words: [Word] = Word.exampleWords
+    @State private var cardPool: [Word] = []
+    
+    
     let layout = [
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
+    
+    @State private var x: [CGFloat] = [0, 0, 0, 0, 0, 0, 0]
+    @State private var degree: [Double] = [0, 0, 0, 0, 0, 0, 0]
 
     var body: some View {
         VStack {
@@ -33,20 +39,76 @@ struct ContentView: View {
             Spacer()
             Divider()
 
-            SwipeCarousel(items: words, id: \.id) { word, size in
-                CardView(title: word.word, description: word.description)
-                    .frame(width: 240, height: 200)
-
+            
+            
+            ZStack {
+                ForEach(0..<6, id: \.self) { card in
+                    
+                    ForEach(cardPool) { word in
+                        
+                        CardView(title: word.word, description: word.description)
+                            .offset(x: self.x[card])
+                            .rotationEffect(.init(degrees: self.degree[card]))
+                            .gesture(DragGesture()
+                                .onChanged({ (value) in
+                                    if value.translation.width > 0 {
+                                        self.x[card] = value.translation.width
+                                        self.degree[card] = 8
+                                    } else {
+                                        self.x[card] = value.translation.width
+                                        self.degree[card] = -8
+                                    }
+                                    
+                                    
+                                })
+                                .onEnded( { (value) in
+                                    if value.translation.width > 0 {
+                                        if value.translation.width > 100 {
+                                            self.x[card] = 500
+                                            self.degree[card] = 15
+                                        } else {
+                                            self.x[card] = 0
+                                            self.degree[card] = 0
+                                        }
+                                    } else {
+                                        if value.translation.width < -100 {
+                                            self.x[card] = -500
+                                            self.degree[card] = -15
+                                        } else {
+                                            self.x[card] = 0
+                                            self.degree[card] = 0
+                                        }
+                                    }
+                                        
+                                        
+                                })
+                            )
+                        
+                    }
+                }
+                
+    //            SwipeCarousel(items: words, id: \.id) { word, size in
+    //                CardView(title: word.word, description: word.description)
+    //                    .frame(width: 240, height: 200)
+    //
+    //            }
+                // Card size specified here, otherwise whole screen will be occupied
+                .frame(width: 220, height: 300)
             }
-            // Card size specified here, otherwise whole screen will be occupied
-            .frame(width: 220, height: 300)
-
+        }
+        .onAppear {
+            cardPool.append(words.randomElement()!)
+            cardPool.append(words.randomElement()!)
+            cardPool.append(words.randomElement()!)
+            cardPool.append(words.randomElement()!)
         }
         .background(
             LinearGradient(colors: [.black, .purple], startPoint: .topLeading, endPoint: .bottomTrailing)
         )
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
