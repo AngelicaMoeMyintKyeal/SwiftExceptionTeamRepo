eed0234b4ad0509bf44ed615b8e5af3c7f1fc5cd
shinra-electric <50119606+shinra-electric@users.noreply.github.com>
2023-01-17 11:35:42 +0100

    Move layout out of VM where it should not have been

--- IdeaBank/IdeaBank/Views/PhaseTwoMainView.swift
+++ IdeaBank/IdeaBank/Views/PhaseTwoMainView.swift
@@ -35,24 +35,6 @@ struct PhaseTwoMainView: View {
                     Spacer()
                 }
             }
-            
-//            VStack {
-//                Text("PHASE 2")
-//                    .font(.subheadline)
-//                    .fontDesign(Font.Design.default)
-//                    .bold()
-//                    .foregroundColor(Color.gray)
-//                Text("Compose")
-//                    .font(.largeTitle)
-//                    .fontDesign(Font.Design.default)
-//                    .bold()
-//                    .foregroundColor(Color.black)
-//                Text("Out of the 20 words you choose, what do these 3 make you think about?")
-//                    .font(.footnote)
-//                    .fontDesign(Font.Design.default)
-//                    .multilineTextAlignment(.center)
-//                    .padding()
-//            }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button("Next Phase") {
