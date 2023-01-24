15d6a5ac8c98c7e5bcbacbea14253d5a67945b6b
Htet Myat Moe Myint Kyeal <htetmyak.m.m.kyal@gmail.com>
2023-01-24 16:01:48 +0100

    created database and regrouped files

--- IdeaBank/IdeaBank/IdeaBankApp.swift
+++ IdeaBank/IdeaBank/IdeaBankApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct IdeaBankApp: App {
+    @StateObject private var dataController = DataController.shared
     @StateObject var viewModel = ViewModel()
     
     var body: some Scene {
