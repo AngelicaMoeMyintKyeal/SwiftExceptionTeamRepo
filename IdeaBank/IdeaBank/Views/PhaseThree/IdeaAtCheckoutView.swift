59b0f4c441b03e043fd4cc75ac56d62bf1325c72
PierSic-dev <113537165+PierSic-dev@users.noreply.github.com>
2023-01-24 16:09:16 +0100

    Added more easter eggs

--- IdeaBank/IdeaBank/Views/PhaseThree/IdeaAtCheckoutView.swift
+++ IdeaBank/IdeaBank/Views/PhaseThree/IdeaAtCheckoutView.swift
@@ -19,61 +19,51 @@ struct IdeaAtCheckoutView: View {
             isToggled.toggle()
         } label: {
             Group {
-
-                    ZStack {
-                        HStack {
-                            VStack(alignment: .leading) {
-                                HStack {
-                                    ForEach(usedWords, id: \.self) {
-                                        Text("\($0)".uppercased())
-                                            .font(.caption)
-                                            .fontWeight(.bold)
-                                            .foregroundColor(.secondary)
-                                    }
+                ZStack {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            HStack {
+                                ForEach(usedWords, id: \.self) {
+                                    Text("\($0)".uppercased())
+                                        .font(.caption)
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.secondary)
                                 }
-                                
-                                Text(title)
-                                    .font(.title)
-                                    .fontWeight(.bold)
-                                    .padding(.bottom, 2.0)
-                                    .foregroundColor(.primary)
-                                
-
-                                    Text(description)
-                            
+                            }
+                            Text(title)
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .padding(.bottom, 2.0)
+                                .foregroundColor(.primary)
+                            Text(description)
                                 .multilineTextAlignment(.leading)
                                 .foregroundColor(.primary)
-                            }
-                            
-                            Spacer()
-                            
-                            isToggled ? Image(systemName: "checkmark.circle.fill") : Image(systemName: "circle")
-
                         }
-                        
-                        .padding(16)
-                        .background(.thinMaterial)
-                        .frame(width: 340)
-                        .cornerRadius(20)
+                        Spacer()
+                        isToggled ? Image(systemName: "checkmark.circle.fill") : Image(systemName: "circle")
                     }
-    //                .overlay(
-    //                    RoundedRectangle(cornerRadius: 20)
-    //                        .stroke(isToggled ? .primary : .secondary, lineWidth: 4)
-    //                        .padding(-2)
-    //                        .opacity(isToggled ? 1 : 0.0)
-    //                )
-                
-
-                
+                    .padding(16)
+                    .background(.thinMaterial)
+                    .frame(width: 340)
+                    .cornerRadius(20)
+                }
+//                .overlay(
+//                    RoundedRectangle(cornerRadius: 20)
+//                        .stroke(isToggled ? .primary : .secondary, lineWidth: 4)
+//                        .padding(-2)
+//                        .opacity(isToggled ? 1 : 0.0)
+//                )
             }
         }
-        
-        
     }
 }
 
 struct IdeaAtCheckoutView_Previews: PreviewProvider {
     static var previews: some View {
-        IdeaAtCheckoutView(usedWords: ["word 1", "word 2", "word 3"], title: "Idea 1", description: "A really cool idea that will make me millions")
+        IdeaAtCheckoutView(
+            usedWords: ["Louie", "Huey", "Dewey"],
+            title: "Roasted Duck",
+            description: "A really cool idea that will feed me"
+        )
     }
 }
