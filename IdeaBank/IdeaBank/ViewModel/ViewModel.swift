831cf68526d09da9d909e7238c8a5bcca3259dc5
PierSic-dev <113537165+PierSic-dev@users.noreply.github.com>
2023-02-03 14:12:23 +0100

    Minor changes

--- IdeaBank/IdeaBank/ViewModel/ViewModel.swift
+++ IdeaBank/IdeaBank/ViewModel/ViewModel.swift
@@ -8,20 +8,11 @@
 import SwiftUI
 
 class ViewModel: ObservableObject {
-    @Published var fetchedWords: [Word] = []
-    @Published var displayingWords: [Word]?
     @Published var selectedWords: [Word] = []
     @Published var words: [Word] = []
     @Published var ideaArray: [Idea] = []
     
-//    private var wordPool: [Word] = Word.exampleWords.shuffled()
-    
     init() {
-//        Task {
-//            for word in wordPool {
-//                await fetchDefinition(randomWord: word.word)
-//            }
-//        }
     }
     
     // Get the index of the user
@@ -32,27 +23,6 @@ class ViewModel: ObservableObject {
         
         return index
     }
-    
-//    @MainActor
-//    func fetchDefinition(randomWord: String) async {
-//        guard let url = URL(string: "https://api.dictionaryapi.dev/api/v2/entries/en/\(randomWord)") else {
-//            return
-//        }
-//
-//        do {
-//            let (data, _) = try await URLSession.shared.data(from: url)
-//            let decodedWords = try JSONDecoder().decode([Word].self, from: data)
-//            if decodedWords.count == 1 {
-//                self.words.append(contentsOf: decodedWords)
-//            } else {
-//                let firstWord = decodedWords[0]
-//                words.append(firstWord)
-//            }
-//        } catch {
-//            print("Catch block!")
-//            print(error.localizedDescription)
-//        }
-//    }
 }
 
 #if DEBUG
