24b53b0558df14cc7ebabd2ddb0d0e45e12124c2
PierSic-dev <113537165+PierSic-dev@users.noreply.github.com>
2023-02-08 10:08:01 +0100

    Minor changes Replaced fetchedWords with words, replaced every reference to exampleWords with pool, created the fillWords method to populate the words property in the initializer and removed exampleWords from the Word struct

--- IdeaBank/IdeaBank/ViewModel/ViewModel.swift
+++ IdeaBank/IdeaBank/ViewModel/ViewModel.swift
@@ -8,32 +8,40 @@
 import SwiftUI
 
 class ViewModel: ObservableObject {
-    @Published var fetchedWords: [Word] = Word.exampleWords.shuffled()
     @Published var displayingWords: [Word] = []
     @Published var selectedWords: [Word] = []
-//    @Published var words: [Word] = Word.exampleWords.shuffled()
+    @Published var words: [Word] = []
     @Published var ideas: [Idea] = []
     @Published var savedIdeas: [Idea] = []
     
-    private var wordCounter: Int = Word.exampleWords.count - 1
+    private var wordCounter: Int = Word.pool.count - 1
     
     init() {
+        fillWords()
         if displayingWords.isEmpty {
             for _ in 0...2 {
-                displayingWords.append(fetchedWords[wordCounter])
+                displayingWords.append(words[wordCounter])
                 wordCounter -= 1
             }
+        }
+    }
+    
+    // This method is required because a property wrapper can not be applied
+    // to a computed property
+    func fillWords() {
+        for wordInPool in Word.pool.shuffled() {
+            words.append(Word(word: wordInPool, definition: ""))
         }
     }
     
     // Function to add new card to displayingWords array
     func addToDisplayingWords() {
         if wordCounter != 0 {
-            displayingWords.append(fetchedWords[wordCounter])
+            displayingWords.append(words[wordCounter])
             wordCounter -= 1
         } else {
-            fetchedWords = fetchedWords.shuffled()
-            wordCounter = Word.exampleWords.count - 1
+            words = words.shuffled()
+            wordCounter = Word.pool.count - 1
         }
     }
     
@@ -54,7 +62,7 @@ extension ViewModel {
         let debugPreview = ViewModel()
         
         for i in 0..<10 {
-            debugPreview.fetchedWords.append(Word.preview(withIndex: i))
+            debugPreview.words.append(Word.preview(withIndex: i))
         }
         
         for i in 0..<3 {
